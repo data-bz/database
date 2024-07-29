4e2a580db133af861be1c03cbfa5d1c7d1daf6c2
ITP Project <krutyev6@gmail.com>
2024-07-29 12:45:32 +0500

    finish1

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const userRouter = require('./routes/user-routes')
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use('/api', userRouter)
 app.use(express.json())
-app.use(cors());
 
 const port = process.env.PORT || 8080
 
