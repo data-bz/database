bdfa402f2c42b766548a2f3f31e68b0f44ac6d60
data-bz <lightrany1@gmail.com>
2024-08-14 22:35:00 +0500

    Update user-routes.js

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,5 +7,5 @@ const userController = require('../controllers/user-controller')
 router.get('/users', userController.getUsers)
 router.get('/profiles', userController.getProfiles)
 router.post('/updateDone', userController.updateDone)
-
+router.get('/messages', userController.getMessages)
 module.exports = router
