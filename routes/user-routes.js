b19517439d436df181bbe1b88467f57cbdaa4a5b
admin <lightrany1@gmail.com>
2024-08-14 22:29:21 +0500

    bbb

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/user-controller')
 
 router.get('/users', userController.getUsers)
 router.get('/profiles', userController.getProfiles)
+router.get('/messages', userController.getMessages)
 router.post('/updateDone', userController.updateDone)
 
 module.exports = router
