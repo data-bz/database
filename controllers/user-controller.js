a760e1c416012d25e0f5088259e850b4a4162bf3
admin <lightrany1@gmail.com>
2024-08-17 10:57:07 +0500

    dsd

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -21,6 +21,27 @@ SET done = ${req.body.done}
 WHERE id = ${req.body.id}`)
         res.json(profiles.rows)
     }
+
+    async sendMessage(req, res) {
+        const { userId, message } = req.body;
+
+        // Вставка нового сообщения в базу данных
+        try {
+            const result = await db.query(
+                `INSERT INTO messages (date_message, message, type, unique_id, user_id)
+                VALUES (NOW(), $1, 'bot', nextval('messages_unique_id_seq'), $2)
+                RETURNING *;`,
+                [message, userId]
+            );
+
+            // Возвращаем добавленное сообщение как ответ
+            res.json(result.rows[0]);
+        } catch (error) {
+            console.error('Ошибка при вставке сообщения:', error);
+            res.status(500).json({ error: 'Ошибка при вставке сообщения в базу данных' });
+        }
+    }
+
 }
 
 module.exports = new UserController()   
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,6 +7,7 @@ const userController = require('../controllers/user-controller')
 router.get('/users', userController.getUsers)
 router.get('/profiles', userController.getProfiles)
 router.get('/messages', userController.getMessages)
+router.post('/sendMessage', userController.sendMessage);
 router.post('/updateDone', userController.updateDone)
 
 module.exports = router
