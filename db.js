b6d3a9f410b173d18afbb31cc97bada2042c5aa4
data-bz <lightrany1@gmail.com>
2024-08-21 19:42:02 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "astrolog", 
-    password: "mypass",
-    host: "5.35.82.254",
+    password: "astRlGGpsswrd",
+    host: "185.179.189.185",
     port: "5432",
     database: "astrologydb",
 })
